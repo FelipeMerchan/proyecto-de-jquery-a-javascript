36831656073e50605aa455cecf9cbfc854154141
Felipe Merchán <felipe-sc@hotmail.es>
2019-06-21 18:44:31 -0500

    Funciones asíncronas

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-fetch('https://randomuser.me/api/sdf')
+/* fetch('https://randomuser.me/api/sdf')
   .then(function (response){
     return response.json()
   })
@@ -7,4 +7,17 @@ fetch('https://randomuser.me/api/sdf')
   })
   .catch(function () {
     console.log('algo salió mal')
-  })
+  }); */
+
+
+(async function load() {
+  async function getData(url) {
+    const response = await fetch(url);
+    const data = await response.json()
+    return data;
+  }
+  const actionList = await getData('https://yts.lt/api/v2/list_movies.json?genre=action');
+  const dramaList = await getData('https://yts.lt/api/v2/list_movies.json?genre=drama');
+  const animationList = await getData('https://yts.lt/api/v2/list_movies.json?genre=animation');
+  console.log(actionList, dramaList, animationList);
+})()

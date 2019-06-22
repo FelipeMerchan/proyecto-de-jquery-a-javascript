92671f42f48e9ec24d16820a63854c7661a72ae9
Felipe Merchán <felipe-sc@hotmail.es>
2019-06-21 22:29:18 -0500

    Reutilizando Funciones

--- js/home.js
+++ js/home.js
@@ -27,23 +27,34 @@
         <figure>
           <img src="${movie.medium_cover_image}" alt="">
         </figure>
-        <p class="primary-list-movie"> ${movie.title}</p>
+        <h4 class="primary-list-movie"> ${movie.title}</h4>
       </div>`
     )
   }
 
-  const $actionContainer = document.querySelector('#action');
-
-  actionList.data.movies.forEach((movie) => {
-    const HTMLString = videoItemTemplate(movie);
+  function createTemplate(HTMLString){
     const html = document.implementation.createHTMLDocument();
     html.body.innerHTML = HTMLString;
-    $actionContainer.append(html.body.children[0]);
-    console.log(HTMLString);
-  })
+    return html.body.children[0];
+  }
+
+  function renderMovieList(list, $container) {
+    $container.children[0].remove();
+    list.forEach((movie) => {
+      const HTMLString = videoItemTemplate(movie);
+      const movieElement = createTemplate(HTMLString);
+      $container.append(movieElement);
+    })
+  }
+
+  const $actionContainer = document.querySelector('#action');
+  renderMovieList(actionList.data.movies, $actionContainer);
 
   const $dramaContainer = document.querySelector('#drama');
+  renderMovieList(dramaList.data.movies, $dramaContainer);
+
   const $animationContainer = document.querySelector('#animation');
+  renderMovieList(animationList.data.movies, $animationContainer);
 
   const $featuringContainer = document.querySelector('#featuring');
   const $form = document.querySelector('#form');
